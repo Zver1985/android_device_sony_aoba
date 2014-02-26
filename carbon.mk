5aba2f3277e11df5e59e5aec07613fc96c4680be
Madhav Kanbur <abcdjdj@gmail.com>
2014-02-26 16:47:32 +0530

    Patch for Carbon Rom

--- carbon.mk
+++ carbon.mk
@@ -16,20 +16,20 @@
 $(call inherit-product, device/sony/aoba/full_aoba.mk)
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_NAME=LT28h \
