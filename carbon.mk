e4e3badba4a63bdcb9fb8f34d6262c733b0c9170
abcdjdj <abcdjdj@gmail.com>
2014-02-27 11:59:28 +0530

    Fixed carbon.mk

--- carbon.mk
+++ carbon.mk
@@ -16,25 +16,25 @@
 $(call inherit-product, device/sony/aoba/full_aoba.mk)
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_NAME=LT28h \
 BUILD_FINGERPRINT="SEMC/LT28h_1267-9107/LT28h:4.0.4/6.1.E.3.7/-PN-zw:user/release-keys" \
 PRIVATE_BUILD_DESC="LT28h-user 4.0.4 6.1.E.3.7 -PN-zw test-keys"
 
-PRODUCT_NAME := cm_aoba
+PRODUCT_NAME := carbon_aoba
 PRODUCT_DEVICE := aoba
